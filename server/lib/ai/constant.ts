d5c62efe27b9a6169d90dd9c0a76b2002a3f95eb
Balerion14 <benaitier.prof.g@gmail.com>
2026-04-16 15:05:23 +0100

    feat(): update modela name

--- server/lib/ai/constant.ts
+++ server/lib/ai/constant.ts
@@ -1,4 +1,4 @@
 import { GoogleGenAI } from "@google/genai";
 
 export const AI_GEMINI_CLIENT = new GoogleGenAI({});
-export const AI_GEMINI_MODEL = "gemini-2.5-flash-lite";
+export const AI_GEMINI_MODEL = "gemini-3-flash-preview";
